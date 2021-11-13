de991466954340bdf086e3dd5a18eadbfd3aafcc
Daniel Strano <stranoj@gmail.com>
2021-11-13 18:38:11 -0500

    Add logo, improve explanation

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import BenchmarkChart from './components/BenchmarkChart.js'
 import Qrack from './Qrack.js'
 import QrackWASM from './Qrack.wasm'
 
+import logo from './assets/img/qrack_logo.png'
+
 const qrack = Qrack({
   locateFile: () => {
     return QrackWASM
@@ -55,11 +57,19 @@ class App extends React.Component {
     return (
       <div className='container text-center'>
         <h1>WebAssembly Qrack with React From Scratch!</h1>
-        <h4>QFT (random unitary initialization)</h4>
+        <h4>QFT (including random unitary initialization, 1 sample)</h4>
         <BenchmarkChart data={this.state.fullData} width={1000} height={400} xLabel='Qubits' xType='number' yLabel='Time (ms)' yType='number' />
         <div>
-          <p>These benchmarks for <a href='https://github.com/vm6502q/qrack'>vm650q/qrack</a> <b>were just run in your browser!</b></p>
-          <p>Qrack can compile for browsers with WebAssembly. In the time it took to load, this page served you a copy of the full Qrack library built for the purpose, ran a quantum Fourier transform, and graphed the timing results!</p>
+          <p>These benchmarks for the <a href='https://github.com/vm6502q/qrack'>vm650q/qrack</a> quantum computer simulator library <b>were just run in your browser!</b></p>
+          <p>Qrack can compile for browsers with WebAssembly. In the time this page took to load, it served you a copy of the full Qrack library built for the purpose, ran a quantum Fourier transform, and graphed the timing results!</p>
+          <p>Qrack also supports GPU workloads with OpenCL, up to multi-device environments. It's the same code to compile!</p>
+        </div>
+        <div className='qrack-logo'>
+          <b>
+            <h4>Powered by Qrack</h4>
+            <img src={logo} width='192px' />
+            <h4 className='qrack-logo-bottom-text'>You rock!</h4>
+          </b>
         </div>
       </div>
     )
