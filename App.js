b9ff340185e3df73d31bdc9aa79890edf8c5a006
Daniel Strano <stranoj@gmail.com>
2021-11-14 15:00:15 -0500

    Shrink D3 chart width

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ class App extends React.Component {
       <div className='container text-center'>
         <h1>WebAssembly Qrack with React From Scratch!</h1>
         <h4>QFT (including random unitary initialization, 1 sample)</h4>
-        <BenchmarkChart data={this.state.fullData} width={1000} height={400} xLabel='Qubits' xType='number' yLabel='Time (ms)' yType='number' />
+        <BenchmarkChart data={this.state.fullData} width={800} height={400} xLabel='Qubits' xType='number' yLabel='Time (ms)' yType='number' />
         <div>
           <p>These benchmarks for the <b>open source</b> <a href='https://github.com/vm6502q/qrack'>vm6502q/qrack</a> quantum computer simulator library <b>were just run in your browser!</b></p>
           <p>Qrack can compile for browsers with WebAssembly. In the time this page took to load, it served you a copy of the full Qrack library built for the purpose, ran a quantum Fourier transform, and graphed the timing results! <a href='https://github.com/WrathfulSpatula/qrack.net'>This page is also open source, as a template.</a></p>
