8ee67097f1f4de861a6108ca57989aa44563b508
Daniel Strano <stranoj@gmail.com>
2021-11-22 18:26:01 -0500

    Update libqrack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, 'index.js'),
