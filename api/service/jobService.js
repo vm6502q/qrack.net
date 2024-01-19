60efcb864823aa62f59b8fd3007816a17244caa0
Daniel Strano <stranoj@gmail.com>
2024-01-18 20:14:31 -0500

    Always over-explain on error.

--- api/service/jobService.js
+++ api/service/jobService.js
@@ -63,9 +63,9 @@ class JobService extends ModelService {
   async invalid_argument_error(job) {
     job.jobStatusTypeId = 2
     job.statusMessage =
-     'All simulator IDs and quantum neuron IDs should be specified as names in output space of job. ' +
-     '(Methods that produce output, at all, always save it to the "output space" of the job, with ' +
-     'variable named by the "output" parameter of the job program line.)'
+     'All simulator IDs and quantum neuron IDs should be specified as names in output space of the job. ' +
+     '(Methods that produce output, at all, always save it to the "output space" of the job, to the' +
+     'variable named by the "output" parameter of the job program line that produces output.)'
     await job.save()
   }
 
