e2bdc18a4bdfb3bf36a4204c61a6c5d6f552d4cc
Daniel Strano <stranoj@gmail.com>
2024-01-20 14:23:57 -0500

    Simplify config.js

--- api/config.js
+++ api/config.js
@@ -7,13 +7,6 @@ config.isDebug = true
 config.app = {}
 config.app.port = process.env.PORT || 8080
 
-config.web = {}
-config.web.url = config.isDebug ? 'localhost:3000' : 'qrack.net'
-config.web.protocol = config.isDebug ? 'http://' : 'https://'
-config.web.getUri = () => {
-  return config.web.protocol + config.web.url
-}
-
 config.api = {}
 config.api.url = config.isDebug ? 'localhost:8080' : 'qrack.net'
 config.api.protocol = config.isDebug ? 'http://' : 'https://'
@@ -36,21 +29,13 @@ config.supportEmail.account = process.env.QRACKNET_SUPPORT_EMAIL_ACCOUNT
 config.supportEmail.password = process.env.QRACKNET_SUPPORT_EMAIL_PASSWORD
 config.supportEmail.address = process.env.QRACKNET_SUPPORT_EMAIL_ADDRESS
 
-config.pgPool = (process.env.QRACKNET_MODE === 'TESTING')
-  ? {
-      user: 'qracknet',
-      host: 'localhost',
-      database: 'qracknetmock',
-      password: process.env.PGPASSWORD,
-      port: 5432
-    }
-  : {
-      user: 'qracknet',
-      host: 'localhost',
-      database: 'qracknet',
-      password: process.env.PGPASSWORD,
-      port: 5432
-    }
+config.pgPool = {
+  user: 'qracknet',
+  host: 'localhost',
+  database: 'qracknet',
+  password: process.env.PGPASSWORD,
+  port: 5432
+}
 
 config.pgConnectionString = 'postgres://' + config.pgPool.user + ':' + config.pgPool.password + '@' + config.pgPool.host + ':' + config.pgPool.port + '/' + config.pgPool.database
 
